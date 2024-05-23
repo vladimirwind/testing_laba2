18126bc6069d3ce42b45ef14df12dcd973f59948
vladimirwind <97838056+vladimirwind@users.noreply.github.com>
2024-05-23 11:46:47 +0300

    add

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let tg = window.Telegram;
+    if(tg.WebApp.initData != undefined && tg.WebApp.initData != "") {
+        tg.WebApp.ready();
+        tg.WebApp.expand();
+    };
+});
