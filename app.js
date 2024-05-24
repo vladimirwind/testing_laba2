360c2d73127f4e51fa793a4df5edbe9400c56741
vladimirwind <97838056+vladimirwind@users.noreply.github.com>
2024-05-24 16:40:05 +0300

    fgsd
    agagag

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     let tg = window.Telegram;
+    particlesJS();
     if(tg.WebApp.initData != undefined && tg.WebApp.initData != "") {
         tg.WebApp.ready();
         tg.WebApp.expand();
+        
     };
 });
