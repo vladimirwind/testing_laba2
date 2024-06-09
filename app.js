b145261fd8a4209cb2b2d906c9bccaaacd3b590d
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-06-09 03:58:53 +0300

    Changing SQL

--- app.js
+++ app.js
@@ -5,6 +5,59 @@ document.addEventListener('DOMContentLoaded', function() {
         tg.WebApp.expand();
         
     };
+    
+    document.getElementById('frensBtn').onclick = function() {
+        sessionStorage.setItem('frensInfo', `{"num_of_refs": 3, "total": 6000, "refs_arr": [{"full_name": "Muhhamad R","league": "br"},{"full_name": "Muhhamad B","league": "sr"},{"full_name": "Muhhamad Z","league": "gd"}], "tops_arr": [{"full_name": "iducky","num": 1232131},{"full_name": "liot","num": 1220},{"full_name": "liot2","num": 120},{"full_name": "liot3","num": 10},{"full_name": "liot4","num": 0}]}`);
+        document.getElementById('mainContainer').style.display = 'none';
+        document.getElementById('frensContainer').style.display = 'flex';
+        let refsTOP = document.getElementById('refsTOP');
+        let frensTOP = document.getElementById('frensTOP');
+
+        refsTOP.style.display = 'none';
+        let refsMenu = document.getElementById('topRefsBtn');
+        let frensMenu = document.getElementById('topFrensBtn');
+        refsMenu.style.boxShadow = 'none';
+
+        let someData = JSON.parse(sessionStorage.getItem('frensInfo'));
+        
+        let topFrensInfo = someData["refs_arr"];
+        let topRefsInfo = someData["tops_arr"];
+
+        for (let ij = 0; ij < topFrensInfo.length; ij++) {
+            let docStrName =  `topFrensName_${ij+1}`
+            // let docStrNumber =  `topReffererNumber_${ij+1}`
+            document.getElementById(docStrName).textContent = topFrensInfo[ij]["full_name"];
+            let imageLeague = document.getElementById(`FrenLeague_${ij+1}`);
+            if (topFrensInfo[ij]["league"] === "sr") {
+                imageLeague.src = './images/Silver_League.png';
+            } else if (topFrensInfo[ij]["league"] === "gd") {
+                imageLeague.src = './images/Golden_League.png';
+            }
+            // document.getElementById(docStrNumber).textContent = topFrensInfo[ij]["num"];
+        }
+
+        for (let ij = 0; ij < topRefsInfo.length; ij++) {
+            let docStrName =  `topReffererName_${ij+1}`
+            let docStrNumber =  `topReffererNumber_${ij+1}`
+            document.getElementById(docStrName).textContent = topRefsInfo[ij]["full_name"];
+            document.getElementById(docStrNumber).textContent = topRefsInfo[ij]["num"];
+        }
+
+        refsMenu.onclick = function() {
+            frensTOP.style.display = 'none';
+            refsTOP.style.display = 'flex';
+            refsMenu.style.boxShadow = "inset #2A4864 0px 0px 40px -9px";
+            frensMenu.style.boxShadow = "none";
+        }
+
+        frensMenu.onclick = function() {
+            frensTOP.style.display = 'flex';
+            refsTOP.style.display = 'none';
+            refsMenu.style.boxShadow = "none";
+            frensMenu.style.boxShadow = "inset #2A4864 0px 0px 40px -9px";
+        }
+    };
+
     sessionStorage.setItem('leaguePage', 1)
     const leagueTrigger = document.getElementById('leagueBtn');
     leagueTrigger.addEventListener('click', function() {
