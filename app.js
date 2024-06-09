36e4b24bb9bec51dfe55d0a9cd73c3a03c2415b8
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-06-09 04:10:50 +0300

    Changing SQL

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
         let topFrensInfo = someData["refs_arr"];
         let topRefsInfo = someData["tops_arr"];
         let BackButton = tg.WebApp.BackButton;
-        
+
         BackButton.show();
         BackButton.onClick(function() {
             BackButton.hide();
             document.getElementById('frensContainer').style.display = 'none';
-            document.getElementById('mainContainer').style.display = 'flex';
+            document.getElementById('mainContainer').style.display = 'block';
         });
 
         for (let ij = 0; ij < topFrensInfo.length; ij++) {
