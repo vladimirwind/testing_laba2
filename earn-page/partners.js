20c16f90206d740c123e5690356470e7c0c71aed
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-09-26 11:18:41 +0300

    fixed

--- earn-page/partners.js
+++ earn-page/partners.js
@@ -3,37 +3,37 @@ const CreateMyMap = () => {
     const AllPartners = new Map();
     AllPartners.set(29, 
         ["https://t.me/cryptobunny_bot?start=invitezANwNdJlYp",
-        "Crypto Bunny",
+        "Play Crypto Bunny",
         "2000",
         "bot"]);
 
     AllPartners.set(28, 
         ["https://t.me/GTap_Off_Bot?start=r_6917022146",
-        "G-Tap",
+        "Play G-Tap",
         "3000",
         "bot"]);
 
     AllPartners.set(34, 
         ["https://t.me/bokgame_bot?start=2077877692",
-        "Kolobok",
+        "Play Kolobok",
         "3000",
         "bot"]);
 
     AllPartners.set(30, 
         ["https://t.me/yourfarmton_bot/farmton?startapp=7JXBG",
-        "Farm TON",
+        "Play Farm TON",
         "3000",
         "bot"]);
 
     AllPartners.set(23, 
         ["https://t.me/hamster_republic_bot?start=userId-6917022146",
-        "Hamster Republic",
+        "Play Hamster Republic",
         "3000",
         "bot"]);
 
     AllPartners.set(33, 
         ["https://t.me/TappySocial_bot/Tappy?startapp=Ku38LL5",
-        "Tappy",
+        "Play Tappy",
         "3000",
         "bot"]);
 
@@ -57,13 +57,13 @@ const CreateMyMap = () => {
 
     AllPartners.set(25, 
         ["https://t.me/itsbrocoinbot/BROSKI?startapp=6917022146",
-        "Bro Coin",
+        "Play Bro Coin",
         "3000",
         "bot"]);
 
     AllPartners.set(31, 
         ["https://t.me/coin_unk_bot/app?startapp=NjkxNzAyMjE0Ng==",
-        "AIRDROP UNK",
+        "Play AIRDROP UNK",
         "3000",
         "bot"]);
 
