9527ce21e2b7b462c822016eb3f5850f8cdff091
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-11-24 22:38:09 +0300

    adding to daily cipher

--- app.js
+++ app.js
@@ -42,8 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('mainContainer').style.display = 'none';
     let profPhoto = tg.WebApp.initDataUnsafe.user.photo_url;
-    console.log("profile_photo:")
-    console.log(profPhoto)
+    document.getElementById('profilePhoto').src = profPhoto;
     let dailyBtn = document.getElementById('dailyComboBtn');
     let dailyBonusBtn = document.getElementById('dailyBonusBtn');
     let dailyCipherBtn = document.getElementById('dailyCipherBtn');
