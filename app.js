5048ff003be6b5ef156bd3587631b06567dc0aca
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-11-26 16:31:03 +0300

    adding to daily cipher

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ document.addEventListener('DOMContentLoaded', function() {
     let Test = async function() {
         const walletsList = await connector.getWallets();
         console.log(JSON.stringify(walletsList))
-        connector.connectWallet();
+        const embeddedWallet = walletsList.find(isWalletInfoCurrentlyEmbedded);
+
+        if (embeddedWallet) {
+            connector.connect({ jsBridgeKey: embeddedWallet.jsBridgeKey });
+            return;
+        }
+        
         const rawAddress = connector.wallet.account.address; // like '0:abcdef123456789...'
         const bouncableUserFriendlyAddress = connector.toUserFriendlyAddress(rawAddress);
         const testnetOnlyBouncableUserFriendlyAddress = toUserFriendlyAddress(rawAddress, true);
