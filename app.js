e9531d895f3a21b8d3c9afb70ebeda52efc38ae5
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-11-28 21:59:49 +0300

    asggggggg

--- app.js
+++ app.js
@@ -147,9 +147,14 @@ document.addEventListener('DOMContentLoaded', function() {
             validUntil: Math.floor(Date.now() / 1000) + 120,
             messages: [
                 {
-                    address: "UQAJtRXS8tpmPFw821L-bSAfY0heLXQCAKpjUkk6kbaL_rZC",
+                    address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
                     amount: "10000000",
-                    payload: "tgid_4121"
+                 // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
+                },
+                {
+                    address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
+                    amount: "20000000",
+                 // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
                 }
             ]
         }
