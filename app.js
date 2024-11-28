d5a80db242493193bc8fd0e59419f28e027220d5
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-11-28 22:00:22 +0300

    asfasf

--- app.js
+++ app.js
@@ -147,12 +147,12 @@ document.addEventListener('DOMContentLoaded', function() {
             validUntil: Math.floor(Date.now() / 1000) + 120,
             messages: [
                 {
-                    address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+                    address: "UQAJtRXS8tpmPFw821L-bSAfY0heLXQCAKpjUkk6kbaL_rZC",
                     amount: "10000000",
                  // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
                 },
                 {
-                    address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
+                    address: "UQAJtRXS8tpmPFw821L-bSAfY0heLXQCAKpjUkk6kbaL_rZC",
                     amount: "20000000",
                  // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
                 }
