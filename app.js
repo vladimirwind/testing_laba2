ab99555464a48889308f022c56a993e9d9436eae
VetrovCCO <VetrovVV2@edu.mos.ru>
2024-12-23 19:00:32 +0300

    asfasf

--- app.js
+++ app.js
@@ -218,9 +218,8 @@ document.addEventListener('DOMContentLoaded', function() {
     cell.bits.writeString(hexString);
     const myBocBytes = cell.toBoc();
 
-    let myHashBase64 = TonWeb.utils.bytesToBase64(cell);
+    let myHashBase64 = TonWeb.utils.bytesToBase64(myBocBytes);
  
-
     console.log(hexString)
 
     buyPremNFTBtn.onclick = async function() {
